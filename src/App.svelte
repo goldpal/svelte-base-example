98003feda6cd97a8f3915ea662bddc16956c4151
goldpal <niclas.kato92@gmail.com>
2023-04-13 11:15:39 +0900

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Button from './components/Button.svelte';
+	export let name;
+
+	const Hello = () => {
+		name = 'Button';
+	};
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+<Button on:click={Hello}>Button</Button>
---
+++ src/components/Button.svelte
@@ -0,0 +1,11 @@
+<style type="text/scss">
+button {
+    font-size: inherit;
+    color: white;
+    border: none;
+    border-radius: 18px;
+    padding: 8px 16px;
+    background-color: gray;
+}
+</style>
+<button on:click><slot/></button>
